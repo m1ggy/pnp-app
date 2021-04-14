5440f9bd72ec5219c5a7aef74c71baad6af8719f
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-04-14 16:25:54 +0800

    finished routing inner switch for dashboard

--- src/components/dashboard components/ManageDownloads.js
+++ src/components/dashboard components/ManageDownloads.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function ChartsMain() {
+export default function ManageDownloads() {
     return (
         <div>
-            charts
+            manage Downloads
         </div>
     )
 }
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,9 +3,17 @@ import {useAuth} from '../contexts/AuthContext'
 import {useHistory, Switch, Route, useRouteMatch} from 'react-router-dom'
 import {Button, Alert, Navbar, Form, Row, Col, Jumbotron} from 'react-bootstrap'
 import Sidebar from '../components/Sidebar'
-import DashboardMain from '../components/dashboard components/DashboardMain.js'
-import AddNewPost from '../components/dashboard components/AddNewPost.js'
-import Drafts from '../components/dashboard components/Drafts.js'
+import DashboardMain from '../components/dashboard components/DashboardMain'
+import AddNewPost from '../components/dashboard components/AddNewPost'
+import Drafts from '../components/dashboard components/Drafts'
+import Published from '../components/dashboard components/Published'
+import ManagePost from "../components/dashboard components/ManagePost";
+import ManageDownloads from '../components/dashboard components/ManageDownloads'
+import DownloadsMain from '../components/dashboard components/DownloadsMain'
+import MapsMain from '../components/dashboard components/MapsMain'
+import ChartsMain from '../components/dashboard components/ChartsMain'
+import AccountsMain from '../components/dashboard components/AccountsMain'
+
 export default function Dashboard() {
 
     const {logout, currentUser} = useAuth()
@@ -21,16 +29,69 @@ export default function Dashboard() {
             setError('Failed to Log out')
         }
     }
+
+
+    ///routes for sidebar
     const routes = () => [
+        <Route 
+        path={`${path}/account`} 
+        key={`${path}/account`}
+        render={()=><AccountsMain/>}
+        //message={"Account"}
+        />,
+        <Route 
+        path={`${path}/charts`} 
+        key={`${path}/charts`}
+        render={()=><ChartsMain/>}
+        //message={"Charts"}
+        />,
+        <Route 
+        path={`${path}/map`} 
+        key={`${path}/map`}
+        render={()=><MapsMain/>}
+        //message={"Map"}
+        />,
+        <Route 
+        path={`${path}/downloads/view-downloads`} 
+        key={`${path}/downloads/view-downloads`}
+        render={()=><DownloadsMain/>}
+        //message={"Downloads"}
+        />,
+        <Route 
+        path={`${path}/downloads/manage-downloads`} 
+        key={`${path}/downloads/manage-downloads`}
+        render={()=><ManageDownloads/>}
+        //message={"Manage Downloads"}
+        />,
+        <Route 
+        path={`${path}/posts/manage-posts`} 
+        key={`${path}/posts/manage-posts`}
+        render={()=><ManagePost/>}
+        //message={"Manage Posts"}
+        />,
+        <Route 
+        path={`${path}/posts/published`} 
+        key={`${path}/posts/published`}
+        render={()=><Published/>}
+        //message={"Published Posts"}
+        />,
         <Route 
         path={`${path}/posts/add-new-post`} 
         key={`${path}/posts/add-new-post`}
-        render={()=>{<AddNewPost/>}}
+        render={()=><AddNewPost/>}
+        //message={"Add New Post"}
+        />,
+        <Route 
+        path={`${path}/posts/drafts`} 
+        key={`${path}/posts/drafts`}
+        render={()=><Drafts/>}
+        //message={"Drafts"}
         />,
         <Route 
         path={`${path}`} 
         key={`${path}`}
-        render={()=>{<DashboardMain/>}}
+        render={()=><DashboardMain/>}
+        // message={"Dashboard"}
         />  
     ]
     
@@ -48,17 +109,16 @@ export default function Dashboard() {
                 <Sidebar/>
                </Col>
                <Col>
-                <Row>
-                    <Jumbotron className="w-100"><h1>test</h1></Jumbotron>
+               
+               <Row>
+                    <Jumbotron className="w-100"><h1></h1></Jumbotron>
+                    </Row>
+                <Row> 
+                    <Switch>               
+                        {routes()}
+                    </Switch>
                 </Row>
-
-                <Row>
-                   <Switch>
-                        
-                    {routes()}
-                      
-                   </Switch>
-                </Row>
+                
                </Col>      
             </Row>
             {error&&<Alert variant="danger">{error}</Alert>} 
