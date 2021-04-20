4d1eec2488e70a4fe1cd7010a7b9986d15f5f8ab
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-04-20 14:58:09 +0800

    centered pnp logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,8 @@ function Header(){
     return(
         <Container fluid className="justify-content-md-center">
              <Row className="d-flex justify-content-center mt-4">
-                <div>
-                <Image/>
-                    <img id="PNPLogo" src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Philippine_National_Police_seal.svg/1200px-Philippine_National_Police_seal.svg.png' alt="PNP Logo"/>
+                <div className="mt-3 mr-3">
+                <Image id="PNPLogo" className="m-auto" src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Philippine_National_Police_seal.svg/1200px-Philippine_National_Police_seal.svg.png' alt="PNP Logo"/>
                     </div>
                     <div id="headerText">
                         <p>Republic of the Philippines</p>
