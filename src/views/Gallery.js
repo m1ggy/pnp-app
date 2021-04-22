8cf18a95e72d7448eeeb51cb59c7f8f7c8352aac
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-04-22 09:36:38 +0800

    added styling for components

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import NavBarMain from '../components/NavBarMain'
 import FooterMain from '../components/FooterMain'
-
+import { Jumbotron, Row, Col, Button, Spinner, Container } from 'react-bootstrap'
 
 function Gallery (){
     return(
         <>
-            <Header/>
-            <NavBarMain/>
-        <h1 className="title">Gallery</h1>
-        <FooterMain/>
+        <Col>
+
+            <Row>
+                <Header  className="mt-2 w-100"/>
+            </Row>
+
+            <Row className="w-100">
+            <Container>
+                <NavBarMain className="mt-2 w-100 m-auto"/>
+                </Container>
+            </Row>
+
+            <Row>
+
+                <Row className="w-100">
+                    <Jumbotron className="mt-2 w-100">
+                        <h1 className="title">Gallery</h1>  
+                    </Jumbotron>
+                </Row>
+
+                <Row>
+
+                </Row>
+
+            </Row>
+
+          
+                <FooterMain/>                             
+        </Col>
     </>
     )
 }
