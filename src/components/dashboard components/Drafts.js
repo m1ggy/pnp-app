6a7886a963e6c7cf9d5861b6cfdb42798dade84c
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-04-26 16:48:38 +0800

    added styling for edit modal

--- src/components/dashboard components/Drafts.js
+++ src/components/dashboard components/Drafts.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react'
-import { Jumbotron, Col, Row, Spinner, Container, Button, Modal, ButtonGroup} from 'react-bootstrap'
+import React, {useEffect, useState, useRef} from 'react'
+import { Jumbotron, Col, Row, Spinner, Container, Button, Modal, ButtonGroup, Form} from 'react-bootstrap'
 import { firestore, storage } from '../../firebase/firebase'
-
+import '../../styles/drafts.css'
 export default function Drafts() {
 
     const [posts,setPosts] = useState([])
@@ -11,10 +11,13 @@ export default function Drafts() {
     const [message, setMessage] = useState()
     const [showMessageModal, setShowMessageModal] = useState(false)
     const [publishModal, setPublishModal] = useState(false)
-
+    const [editModal, setEditModal] = useState(false)
     const db = firestore.collection('posts')
     const storageRef = storage.ref('images')
-
+    const contentRef = useRef()
+    const titleRef = useRef()
+    const subtitleRef = useRef()
+    const publishRef = useRef()
 
     function RenderPosts (){
         if(posts === null|| typeof posts === undefined) return null;
@@ -30,7 +33,7 @@ export default function Drafts() {
         return(
             posts.map((post, index)=>{
                 return(
-                    <Row key={index} className="border p-3"> 
+                    <Row key={index} className="border p-3 w-100"> 
 
                             <Col lg={10}>
                                 <Row><h3>{post.title}</h3></Row>
@@ -41,7 +44,7 @@ export default function Drafts() {
                             <Col className="m-auto">
                                 <Row>
                                     <Button variant="primary" size="sm" className="m-3 w-75" onClick={()=>{setPublishModal(true);setSelectedItem(post)}}>Publish</Button>
-                                    <Button variant="info" size="sm" className="m-3 w-75">Edit</Button>                 
+                                    <Button variant="info" size="sm" className="m-3 w-75" onClick={()=>{setEditModal(true);setSelectedItem(post)}}>Edit</Button>                 
                                     <Button variant="danger" size="sm" className="m-3 w-75" onClick={()=>{setShowModal(true);setSelectedItem(post)}}>Delete</Button>
                                 </Row>                                                                           
                             </Col>             
@@ -124,6 +127,91 @@ export default function Drafts() {
         setSelectedItem()
         setShowMessageModal(true)
     }
+
+    function editPost(){
+
+    }
+
+
+
+    function EditModal(){
+        
+        if(selectedItem === null||typeof selectedItem === 'undefined')
+        {return null;}
+
+
+        titleRef.current = selectedItem.title
+        subtitleRef.current = selectedItem.subtitle
+        contentRef.current = selectedItem.content
+        return(
+            <Modal 
+            show={editModal} 
+            onHide={()=>{setEditModal(false)}} 
+            centered
+            dialogClassName="edit-modal"
+            >
+                <Modal.Header>
+                 <h3>
+                    Edit Post
+                    </h3>
+                </Modal.Header>
+                <Modal.Body>
+
+                 <Form onSubmit={editPost}>
+
+                <Form.Group>
+                    <Form.Label>
+                    <b>Enter Title</b>
+                    </Form.Label>
+                    <Form.Control type="text" placeholder="Title" required style={{width:"50%"}} className="border" value={titleRef.current}/>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>
+                    <b>Enter Subtitle</b>
+                    </Form.Label>
+                    <Form.Control type="text" placeholder="Subtitle" required style={{width:"75%"}} className="border" value={ subtitleRef.current}/>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>
+                        <b>Enter Content </b>
+                    </Form.Label>
+                    
+                    <Form.Control as="textarea" required rows={25}style={{resize:'none', width:"100%"}} className="border" value={contentRef.current}/>
+                </Form.Group>
+
+                <Form.Check 
+                    type="switch"
+                    id="custom-switch"
+                    label="Publish?"
+                    ref={publishRef}   
+                    className="mt-5 mb-3"            
+                />
+
+               
+                <Container className="mt-5">
+                    <Button variant="primary" type="submit" className="mt-5 w-25 m-auto" size="md">
+                        Submit
+                    </Button>
+                </Container>
+               
+
+                </Form>
+                   
+                </Modal.Body>
+                <Modal.Footer>
+                <ButtonGroup>
+                    <Button variant="danger" size="sm" className="w-75 m-3" onClick={()=>{setEditModal(false)}}>
+                        Cancel
+                    </Button>
+                    </ButtonGroup>
+                   
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+    
 
     function DeleteModal(){
 
@@ -223,13 +311,14 @@ export default function Drafts() {
             </Row> 
 
             <Row>
-            <Jumbotron className="w-100" style={{display:'flex', justifyContent:'center'}}>
+            <Jumbotron className="w-100">
                
                  {posts?<RenderPosts/>:<p>No posts in drafts.</p>}
                  {loading?<Container><Spinner animation="border" className="m-auto"/></Container>:null}
                 <DeleteModal/>
                 <MessageModal/>
                 <PublishModal/>
+                <EditModal/>
             </Jumbotron>     
           
             </Row>
