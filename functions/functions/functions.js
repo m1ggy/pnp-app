f10fe255621ae7756bf39f4ed408e5d3de63dc5f
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-05-24 14:38:58 +0800

    testing

--- functions/functions/functions.js
+++ functions/functions/functions.js
@@ -1,40 +1,39 @@
-import * as admin from 'firebase-admin';
-import adminKey from './adminKey.json';
+// import * as admin from 'firebase-admin';
+// import adminKey from './adminKey.json';
 
-const adminapp = admin.initializeApp({
-  credential: admin.credential.cert(adminKey),
-});
+// const adminapp = admin.initializeApp({
+//   credential: admin.credential.cert(adminKey),
+// });
 
-const adminAuth = adminapp.auth();
+// const adminAuth = adminapp.auth();
 
-function createUser(email, password) {
-  adminAuth
-    .createUser({ email, password })
-    .then(() => {
-      return new Promise((resolve) => {
-        resolve(JSON.stringify({ message: 'Successfully created account.' }));
-      });
-    })
-    .catch(() => {
-      return new Promise((resolve) => {
-        resolve(JSON.stringify({ message: 'failed to create account.' }));
-      });
-    });
-}
+// function createUser(email, password) {
+//   adminAuth
+//     .createUser({ email, password })
+//     .then(() => {
+//       return new Promise((resolve) => {
+//         resolve(JSON.stringify({ message: 'Successfully created account.' }));
+//       });
+//     })
+//     .catch(() => {
+//       return new Promise((resolve) => {
+//         resolve(JSON.stringify({ message: 'failed to create account.' }));
+//       });
+//     });
+// }
 
-exports.handler = async function (event) {
-  let { email, password } = event.body;
-  console.log('INSIDE THE FUNCTION!!!');
-  if (event) {
-    createUser(email, password).then((res) => {
-      let message = JSON.parse(res.message);
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message }),
-      };
-    });
-  }
-
+exports.handler = async function () {
+  // let { email, password } = event.body;
+  // console.log('INSIDE THE FUNCTION!!!');
+  // if (event) {
+  //   createUser(email, password).then((res) => {
+  //     let message = JSON.parse(res.message);
+  //     return {
+  //       statusCode: 200,
+  //       body: JSON.stringify({ message }),
+  //     };
+  //   });
+  // }
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'THE FUNCTION IS WORKING!!!!' }),
