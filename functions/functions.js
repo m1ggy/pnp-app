8d4e5a614c0e13244949321499ea431d57a78e5b
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-05-24 13:29:34 +0800

    adding netlify functions

---
+++ functions/functions.js
@@ -0,0 +1,35 @@
+import * as admin from 'firebase-admin';
+import adminKey from './adminKey.json';
+
+const adminapp = admin.initializeApp({
+  credential: admin.credential.cert(adminKey),
+});
+
+const adminAuth = adminapp.auth();
+
+function createUser(email, password) {
+  adminAuth
+    .createUser({ email, password })
+    .then(() => {
+      return new Promise((resolve) => {
+        resolve(JSON.stringify({ message: 'Successfully created account.' }));
+      });
+    })
+    .catch(() => {
+      return new Promise((resolve) => {
+        resolve(JSON.stringify({ message: 'failed to create account.' }));
+      });
+    });
+}
+
+exports.handler = async function (event) {
+  let { email, password } = event.body;
+
+  createUser(email, password).then((res) => {
+    let message = JSON.parse(res.message);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message }),
+    };
+  });
+};
