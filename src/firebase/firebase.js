571d764f026763b0205336d9f488cd9637202c50
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-08-13 19:36:56 +0800

    z

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 export const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN_FIREBASE,
-  projectId: process.env.REACT_APP_PROJECT_ID_FIREBASE,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_FIREBASE,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_FIREBASE,
-  appId: process.env.REACT_APP_APP_ID_FIREBASE,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID_FIREBASE,
+  apiKey: 'AIzaSyBwViIzOoZjdG2uGjsh42SDP63JsDXG3pI',
+  authDomain: 'pnp-app-a046a.firebaseapp.com',
+  projectId: 'pnp-app-a046a',
+  storageBucket: 'pnp-app-a046a.appspot.com',
+  messagingSenderId: '857568307959',
+  appId: '1:857568307959:web:622a285999bfc8e82641f2',
 });
 export const firebaseAuth = firebaseApp.auth();
 export const firestore = firebase.firestore();
